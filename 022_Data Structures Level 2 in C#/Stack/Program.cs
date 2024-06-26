424586f06802d878d15b168da0757e0d66ff3adb
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-06-26 07:14:42 +0300

    Add Stack

---
+++ 022_Data Structures Level 2 in C#/Stack/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            // Pushing elements onto the stack
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            // Peeking at the top element
+            Console.WriteLine("Top element: " + stack.Peek());
+
+            //All Element in the stack
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Popping elements from the stack
+            Console.WriteLine("Popped: " + stack.Pop());
+            Console.WriteLine("Popped: " + stack.Pop());
+
+            // Checking if the stack is empty
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Stack is empty.");
+            }
+            else
+            {
+                Console.WriteLine("Top element: " + stack.Peek());
+            }
+
+            // Clearing the stack
+            stack.Clear();
+
+            Console.ReadKey();
+        }
+    }
+}
