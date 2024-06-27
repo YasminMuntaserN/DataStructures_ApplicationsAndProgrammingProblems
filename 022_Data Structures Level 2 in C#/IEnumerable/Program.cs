4b382d00cf30c469105890d6c0a500bf776e2f58
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-06-27 09:43:04 +0300

    Add IEnumerable

---
+++ 022_Data Structures Level 2 in C#/IEnumerable/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class CustomCollection<T> : IEnumerable<T>
+    {
+        private List<T> _Items = new List<T>();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T Item in _Items)
+            {
+                yield return Item;
+            }
+        }
+
+ 
+        public void Add(T Item)
+        {
+            _Items.Add(Item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        
+        }
+}
+    internal class Program
+    {
+        static void Main(string[] args)
+    {
+
+        CustomCollection<int> Numbers = new CustomCollection<int>();
+
+        Numbers.Add(1);
+        Numbers.Add(2);
+        Numbers.Add(3);
+
+        foreach (int i in Numbers)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadKey();
+
+    }
+    }
+    
