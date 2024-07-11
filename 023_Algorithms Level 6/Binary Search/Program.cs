9e58d948b767f1ac3bdda2fd88f62591d6091982
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-07-11 16:15:26 +0300

    feat: add Binary Search

---
+++ 023_Algorithms Level 6/Binary Search/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class BinarySearchExample
+    {
+        static int BinarySearch(int[] arr, int x)
+        {
+            int Start = 0, End = arr.Length - 1;
+            int Trials = 0;
+
+
+            while (Start <= End)
+            {
+                int Middle = Start + (End - Start) / 2;
+                Trials++;
+
+                Console.WriteLine("Trial = " + Trials);
+
+                // Check if x is present at mid
+                if (arr[Middle] == x)
+                    return Middle;
+
+                // If x greater, ignore left half
+                if (x > arr[Middle])
+                    Start = Middle + 1;
+
+                // If x is smaller, ignore right half
+                else
+                    End = Middle - 1;
+            }
+
+            // If we reach here, then element was not present
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 22, 25, 37, 41, 45, 46, 49, 51, 55, 58, 70, 80, 82, 90, 95 }; // Sorted array
+
+            int x = 45; // Element to be searched
+
+            Console.WriteLine("Sorted Array:");
+            foreach (var item in arr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            int result = BinarySearch(arr, x);
+
+            if (result == -1)
+                Console.WriteLine("Element not found in the array.");
+            else
+                Console.WriteLine("Element found at index: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
